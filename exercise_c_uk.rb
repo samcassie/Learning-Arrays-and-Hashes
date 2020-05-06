01d34f398cfa1a5bed66c4ab221f0b31a7af9182
samcassie <samgcassie@gmail.com>
2020-05-06 21:53:04 +0100

    uploading homework

---
+++ exercise_c_uk.rb
@@ -0,0 +1,52 @@
+united_kingdom = [
+  {
+    name: "Scotland",
+    population: 5295000,
+    capital: "Edinburgh"
+  },
+  {
+    name: "Wales",
+    population: 3063000,
+    capital: "Swansea"
+  },
+  {
+    name: "England",
+    population: 53010000,
+    capital: "London"
+  }
+]
+
+# 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
+
+united_kingdom[1][:capital] = "Cardiff"
+p united_kingdom[1]
+
+# 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
+
+northern_ireland = {
+    name: "Northern Ireland",
+    population: 3000000,
+    capital: "Belfast"
+}
+
+united_kingdom.append(northern_ireland)
+p united_kingdom
+
+
+# 3. Use a loop to print the names of all the countries in the UK.
+
+for country in united_kingdom
+    p country[:name]
+end
+
+# 4. Use a loop to find the total population of the UK.
+
+def total_pop(array)
+    total = 0
+        for country in array
+            total += country[:population]
+        end
+    p total
+end
+
+total_pop(united_kingdom)
